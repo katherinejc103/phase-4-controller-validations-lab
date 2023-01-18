e7e990f2c8ad4521c9dce5a424da28d764f13a71
Katherine <katherinejc103@gmail.com>
2023-01-17 22:54:14 -0500

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+
+    def create
+        author = Author.create(author_params)
+        if author.valid?
+            render json: author, status: :created
+        else
+            render json: { error: author.errors }, status: :unprocessable_entity
+        end
+    end
+
 end
